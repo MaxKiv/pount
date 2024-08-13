d521a9c2d61a5267280ef3f6ebc5a07f224a099a
MaxKiv <maxkivits42@gmail.com>
2024-08-13 10:49:45 +0200

    feat(init): initialised from bevy template
    from: https://blog.graysonhead.net/posts/nix-flake-rust-bevy/

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::BLACK))
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, spawn_scene)
+        .add_systems(Update, rotate_scene)
+        .run();
+}
+
+#[derive(Reflect, Component)]
+pub struct Rotate {}
+
+fn spawn_scene(mut commands: Commands, assets: Res<AssetServer>) {
+    let lighthouse = assets.load("lighthouse.glb#Scene0");
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 300.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        ..default()
+    });
+    commands
+        .spawn(SceneBundle {
+            scene: lighthouse,
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            ..Default::default()
+        })
+        .insert(Rotate {});
+    let mut camera_transform = Transform::from_xyz(-5.0, 2.0, -5.0);
+    let camera_direction =
+        (Transform::from_xyz(0.0, 0.0, 0.0).translation - camera_transform.translation).normalize();
+    camera_transform.look_at(camera_direction, Vec3::Y);
+    commands.spawn(Camera3dBundle {
+        transform: camera_transform,
+        ..Default::default()
+    });
+}
+
+fn rotate_scene(mut scene_object: Query<(Entity, &Rotate, &mut Transform)>) {
+    for (_, _, mut transform) in &mut scene_object {
+        transform.rotate_y(0.05);
+    }
+}
