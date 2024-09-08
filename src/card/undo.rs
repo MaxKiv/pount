38c3e32705e82ac19e690289e25a349b0729dcf5
MaxKiv <maxkivits42@gmail.com>
2024-09-08 02:21:44 +0200

    feat(undo): refactor undo.rs

--- src/card/undo.rs
+++ src/card/undo.rs
@@ -25,14 +25,13 @@ pub struct UndoContext<'w> {
     pub keymap: Res<'w, KeyMap>,
 }
 
-#[allow(clippy::too_many_arguments)]
 pub fn undo_last_move(
     mut commands: Commands,
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut context: UndoContext,
 ) {
-    if should_undo(&keyboard_input, &context) {
-        if let Some(placed_cards) = &mut context.card_history.0 {
+    if should_undo(&keyboard_input, &context.keymap) {
+        if let Some(placed_cards) = context.card_history.0.as_mut() {
             handle_undo(
                 &mut commands,
                 placed_cards,
@@ -56,27 +55,11 @@ fn handle_undo(
         entity,
     }) = placed_cards.pop()
     {
-        let (x, y, _) = board_coordinates.as_xys();
-
         // despawn player win notification if previous move won the game
-        if let Some(entity) = player_win_entity.0 {
-            info!("despawning player win notification {:?}", entity);
-            commands.entity(entity).despawn_recursive();
-            player_win_entity.0 = None;
-        }
+        despawn_win_notification(commands, player_win_entity);
 
         // Remove last placed card from gamestate
-        board_state
-            .get_tile_mut(x, y)
-            .cards
-            .pop()
-            .expect("Attempting to remove the previously placed card, but no card found");
-
-        // Undo of the first move: update board state empty flag
-        if placed_cards.is_empty() {
-            info!("setting board state empty");
-            board_state.empty = true;
-        }
+        undo_card_placement(board_state, board_coordinates, placed_cards);
 
         // Despawn last entity
         commands.entity(entity).despawn_recursive();
@@ -86,10 +69,37 @@ fn handle_undo(
     }
 }
 
-fn should_undo(keyboard_input: &ButtonInput<KeyCode>, context: &UndoContext) -> bool {
+fn undo_card_placement(
+    board_state: &mut GameState,
+    board_coordinates: BoardCoordinates,
+    placed_cards: &[PlacedCard],
+) {
+    let (x, y, _) = board_coordinates.as_xys();
+
+    board_state
+        .get_tile_mut(x, y)
+        .cards
+        .pop()
+        .expect("Attempting to remove the previously placed card, but no card found");
+
+    // Undo of the first move: update board state empty flag
+    if placed_cards.is_empty() {
+        info!("setting board state empty");
+        board_state.empty = true;
+    }
+}
+
+fn despawn_win_notification(commands: &mut Commands, player_win_entity: &mut PlayerWinEntity) {
+    if let Some(entity) = player_win_entity.0 {
+        info!("despawning player win notification {:?}", entity);
+        commands.entity(entity).despawn_recursive();
+        player_win_entity.0 = None;
+    }
+}
+
+fn should_undo(keyboard_input: &ButtonInput<KeyCode>, keymap: &KeyMap) -> bool {
     keyboard_input.just_pressed(
-        context
-            .keymap
+        keymap
             .0
             .get("undo")
             .cloned()
